3eaf98aa10e28d4c0b534180b75d48e9d28ea9d4
Alexander <jalexander9609@gmail.com>
2020-07-06 12:08:33 -0500

    Carpeta config General

---
+++ app/src/main/java/com/wakeup/bandsdk/configVar/ConfigGeneral.java
@@ -0,0 +1,8 @@
+package com.wakeup.bandsdk.configVar;
+
+public class ConfigGeneral {
+
+    public static final String URL_BASE="https://sura-4.herokuapp.com/api";
+    public static final String TYPE = "application/json; charset=utf-8";
+
+}
