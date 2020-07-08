17072788b9dfba1ef10d3e20516c7e56069f76c2
Andres Fernando Saa Montaño <afsaa@utp.edu.co>
2020-07-08 15:45:51 -0500

    Organizando flujo de actividades. Obteniendo datos de usuario por Id

--- app/src/main/java/com/wakeup/bandsdk/activity/LoginActivity.java
+++ app/src/main/java/com/wakeup/bandsdk/activity/LoginActivity.java
@@ -3,17 +3,24 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.gson.JsonObject;
 import com.wakeup.bandsdk.Pojos.Authenticate.JWTAuth;
+import com.wakeup.bandsdk.Pojos.Fisiometria.DataFisiometria;
 import com.wakeup.bandsdk.R;
 import com.wakeup.bandsdk.Services.ServiceFisiometria;
 import com.wakeup.bandsdk.configVar.ConfigGeneral;
+import com.wakeup.bandsdk.activity.HomeActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Button directAccessBtn = findViewById(R.id.directAccess);
         Button loginBtn = findViewById(R.id.loginBtn);
         Button toRegisterBtn = findViewById(R.id.toRegisterBtn);
 
@@ -40,6 +48,11 @@ public class LoginActivity extends AppCompatActivity {
         loginUsernameInput = findViewById(R.id.loginUsernameText);
         loginUsernameField = findViewById(R.id.loginUsernameField);
         loginPasswordInput = findViewById(R.id.loginPasswordText);
+
+        directAccessBtn.setOnClickListener(v -> {
+            Intent homeIntent = new Intent(this, HomeActivity.class);
+            startActivity(homeIntent);
+        });
 
         loginUsernameInput.addTextChangedListener(new TextWatcher(){
             @Override
@@ -85,12 +98,15 @@ public class LoginActivity extends AppCompatActivity {
 
         loginBtn.setOnClickListener( v -> {
             Log.d(TAG, "User credentials -> " + userCredentials);
-            getJwtToken(userCredentials);
+            getJwtToken(this, userCredentials);
         });
-        toRegisterBtn.setOnClickListener(v -> setContentView(R.layout.activity_register));
+        toRegisterBtn.setOnClickListener(v -> {
+            Intent registerIntent = new Intent(this, RegisterActivity.class);
+            startActivity(registerIntent);
+        });
     }
 
-    public void getJwtToken(JsonObject credentials) {
+    public void getJwtToken(Context loginContext, JsonObject credentials) {
         ServiceFisiometria service = ConfigGeneral.retrofit.create(ServiceFisiometria.class);
         final Call<JWTAuth> responseData = service.getJwtToken(credentials);
 
@@ -100,6 +116,7 @@ public class LoginActivity extends AppCompatActivity {
                 if (response.isSuccessful()){
                     assert response.body() != null;
                     Log.i(TAG, "onResponse: " + response.body().toString());
+                    getPhysiometryDataById(loginContext, response.body().getIdToken(), 3);
                 }
                 if (response.code() == 401) {
                     loginUsernameField.setError("Credenciales inválidas. Intentalo de nuevo.");
@@ -109,6 +126,33 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<JWTAuth> call, Throwable t) {
+                System.out.println(t.getMessage());
+            }
+        });
+    }
+
+    public void getPhysiometryDataById(Context loginContext, String jwtToken, int userId) {
+        ServiceFisiometria service = ConfigGeneral.retrofit.create(ServiceFisiometria.class);
+        final Call<List<DataFisiometria>> dataResponse = service.getStudiesSubjes("Bearer " + jwtToken, userId);
+
+        dataResponse.enqueue(new Callback<List<DataFisiometria>>() {
+            @Override
+            public void onResponse(Call<List<DataFisiometria>> call, Response<List<DataFisiometria>> response) {
+                if (response.isSuccessful()) {
+                    List<DataFisiometria> res = response.body();
+                    assert res != null;
+                    System.out.println("Physiometry data size for this user -> " + res.size());
+                    for (DataFisiometria c : res) {
+                        System.out.println("User Id -> " + c.getdataUser().getId());
+                        System.out.println("User email -> " + c.getdataUser().emailget());
+                    }
+                    Intent homeIntent = new Intent(loginContext, HomeActivity.class);
+                    startActivity(homeIntent);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<DataFisiometria>> call, Throwable t) {
                 System.out.println(t.getMessage());
             }
         });
