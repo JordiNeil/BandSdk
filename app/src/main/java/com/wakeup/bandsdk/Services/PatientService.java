171757c51bd253a32ea3f259e9b6eaea82ddd4fc
Andres Fernando Saa Montaño <afsaa@utp.edu.co>
2020-07-15 10:18:52 -0500

    Agregando las interfaces de servicio para paciente, condicion e ips

--- app/src/main/java/com/wakeup/bandsdk/Services/PatientService.java
+++ app/src/main/java/com/wakeup/bandsdk/Services/PatientService.java
@@ -1,5 +1,14 @@
 package com.wakeup.bandsdk.Services;
 
-public interface PatientService {
+import com.google.gson.JsonObject;
+import com.wakeup.bandsdk.Pojos.Patients.PatientData;
 
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface PatientService {
+    @POST("pacientes")
+    Call<PatientData> setPatient(@Header("Authorization") String api_token, @Body JsonObject body);
 }
